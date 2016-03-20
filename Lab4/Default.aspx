a05925ffb75bcc2b0d00537ae38d4ca3d7556225
Zack Ganger <zganger@bu.edu>
2016-03-20 18:23:56 -0400

    initial commit

---
+++ Lab4/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Unit Converter</title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 66px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <center><table><tr><td colspan="2">
+    <h1 style="text-align: center;">Unit Conversion</h1></tr>
+        <tr>
+            <td colspan="2"><asp:TextBox ID="num1" runat="server"></asp:TextBox><asp:DropDownList ID="fromList" runat="server"></asp:DropDownList> = <asp:TextBox ID="num2" runat="server" ReadOnly="True"></asp:TextBox><asp:DropDownList ID="toList" runat="server"></asp:DropDownList></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align:center; padding-top:25px;">
+                <asp:Button ID="Convert" runat="server" Text="Convert" OnClick="Convert_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align:center; padding-top:25px;" class="auto-style1"><asp:TextBox ID="ErrorBox" runat="server" style="text-align:center" Visible="False" ReadOnly="True" Width="346px"></asp:TextBox></td>
+        </tr>
+    </table></center>
+    </form>
+</body>
+</html>
---
+++ Lab4/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected string[] units = { "m", "cm", "mm", "km", "mi", "yd", "ft", "in" };
+    protected double[] conv = { 1, .01, .001, 1000, 1609.34, 0.9144, 0.3048, 0.0254 };
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            fromList.DataSource = units;
+            toList.DataSource = units;
+            fromList.DataBind();
+            toList.DataBind();
+        }
+    }
+
+    protected void Convert_Click(object sender, EventArgs e)
+    {
+        double innum;
+        ErrorBox.Visible = false;
+        int inindex = fromList.SelectedIndex;
+        int outindex = toList.SelectedIndex;
+        if (inindex == outindex)
+        {
+            num1.Text = "";
+            ErrorBox.Text = "You're converting to the same unit! Try again.";
+            ErrorBox.Visible = true;
+        }
+        else
+        {
+            if (Double.TryParse(num1.Text, out innum))
+            {
+                ErrorBox.Visible = false;
+                num2.Text = ((innum * conv[inindex]) / conv[outindex]).ToString();
+            }
+            else
+            {
+                num1.Text = "";
+                ErrorBox.Text = "Please enter a valid number!";
+                ErrorBox.Visible = true;
+            }
+        }
+    }
+}
